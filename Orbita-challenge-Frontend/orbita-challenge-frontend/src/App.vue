a0422bf36f9ef06cf20f3c1804552406f6168836
Edmiguel-BR <59443049+Edmiguel-BR@users.noreply.github.com>
2022-10-24 01:13:05 -0300

    :sparkles: Criação do projeto front-end

---
+++ Orbita-challenge-Frontend/orbita-challenge-frontend/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="#F2F2F2"
+      dark
+      height="100"
+    >
+    <div class="">
+        <v-img
+          alt="Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/mais_a.png"
+          transition="scale-transition"
+          width="100"
+        />
+      </div>
+    </v-app-bar>
+    <v-main>
+      <AcademicModule/>      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AcademicModule from './components/AcademicModule.vue';
+
+export default {
+  name: 'App',
+
+  components: {    
+    AcademicModule,
+},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ Orbita-challenge-Frontend/orbita-challenge-frontend/src/components/AcademicModule.vue
@@ -0,0 +1,42 @@
+<template>  
+  <v-layout>
+        
+    <v-navigation-drawer>
+      <v-toolbar fixed dark>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>
+              Módulo Acadêmico
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    
+    <AlunosGrid />
+  </v-layout>
+</template>
+
+<script>
+  import AlunosGrid from './AlunosGrid.vue';
+  export default {
+  components: { AlunosGrid },  
+    name: 'AcademicModule',
+    data: () => ({
+      items: [
+      { title: 'Alunos', icon: 'mdi-account-box' },
+    ],      
+    }),
+  }
+</script>
